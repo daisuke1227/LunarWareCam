63024a8e1445469671863b2355aa02b967003c55
daisuke1227 <144384874+daisuke1227@users.noreply.github.com>
2024-04-01 17:28:28 -0400

    Create Lunar.lua

---
+++ Lunar.lua
@@ -0,0 +1,25 @@
+local UserInputService = game:GetService("UserInputService")
+local isMobile = UserInputService.TouchEnabled
+
+if isMobile then
+    getgenv().keytoclick = "Q"
+    local tool = Instance.new("Tool")
+    tool.RequiresHandle = false
+    tool.Name = keytoclick
+    tool.Activated:Connect(function()
+        local vim = game:GetService("VirtualInputManager")
+        vim:SendKeyEvent(true, keytoclick, false, game)
+    end)
+    tool.Parent = game.Players.LocalPlayer.Backpack
+end
+
+wait(0.2)
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))()
+local Notify = AkaliNotif.Notify
+Notify({
+    Description = "lunar ware",
+    Title = "lunarware top",
+    Duration = 3
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/daisuke1227/l/main/Lunar.lua"))()
